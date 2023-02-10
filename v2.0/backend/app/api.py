013a6f7b26ce2b5cb789eccc7a0d32d7a1e7d83a
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-02-10 09:46:57 +0800

    Add Oauth2 endpoints and set/get cookies
    1. Add OAuth2 authentication in endpoints /token
    2. Use Oatuh2 authorization in method get_current_user
    3. add set/get cookie endpoints

--- v2.0/backend/app/api.py
+++ v2.0/backend/app/api.py
@@ -1,10 +1,20 @@
-from typing import Union
+from typing import Union, Optional
 from fastapi import FastAPI, Depends, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
 
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import engine, get_db
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from .utils import hash_password, verify_password
+
+from fastapi import Cookie, Response
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -23,6 +33,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
@@ -157,7 +169,145 @@ def user_logout(body: schemas.UserLogout, db: Session = Depends(get_db)):
     crud.update_is_active(db, db_user, False)
     return
 
-    
+"""Cookie test"""
+@app.get("/set")
+async def set_cookie(response: Response):
+    response.set_cookie(key="refresh_token", value="username", max_age=300)
+    return True
 
+@app.get("/get")
+async def get_cookie(refresh_token: Optional[str] = Cookie(None)):
+    print(refresh_token)
+    return True
+
+
+""" OAuth2 Authentication and Authorization"""
+# tokenUrl is the URL the client use to send username and password to get a token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# automatically check the Authorization header
+@app.get("/test/")
+async def read_tokens(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+class fake_User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+class fake_UserInDB(fake_User):
+    hashed_password: str
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return fake_UserInDB(**user_dict)
+
+# def fake_decode_token(token):
+#     # This doesn't provide any security at all
+#     # Check the next version
+#     user = get_user(fake_users_db, token)
+#     return user
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    return user
+
+async def get_current_active_user(active_user: fake_User = Depends(get_current_user)):
+    if active_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return active_user
+
+@app.get("/test/me")
+async def read_me(user: fake_User = Depends(get_current_active_user)):
+    return user
+
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+SECRET_KEY = "871796f3497fd7ce65efd5d475c794a4723d2907b3c2b3730ca838070ca816d3"
+ALORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 5
+
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str
+
+# class TokenDate(BaseModel):
+#     username: Union[str, None] = None
 
 
--- v2.0/backend/app/crud.py
+++ v2.0/backend/app/crud.py
@@ -71,3 +71,8 @@ def update_is_active(db: Session, user: User, is_active: bool):
     user.is_active = is_active
     db.commit()
     return
+
+"""OAuth2"""
+def get_user(db: Session, username) -> User:
+    db_user = db.query(User).filter(User.username == username).first()
+    return db_user

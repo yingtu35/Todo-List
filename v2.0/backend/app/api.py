175b0905aa068b9dccaa400bad4751c7c0c3ed30
yingtu35 <91551415+yingtu35@users.noreply.github.com>
2023-02-27 16:02:22 +0800

    Delete unused code and comment

--- v2.0/backend/app/api.py
+++ v2.0/backend/app/api.py
@@ -9,10 +9,10 @@ from .database import engine, get_db
 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from .utils import hash_password, verify_password
+from .credentials import SECRET_KEY, ALORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 from fastapi import Cookie, Response
 from .models import User
@@ -37,6 +37,8 @@ app.add_middleware(
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# TODO: Handle token expiration and redirect user to login page
 
 async def current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
@@ -109,21 +111,12 @@ def delete_item(id: int, db: Session = Depends(get_db), user: User = Depends(cur
     crud.delete_item(db, db_item)
     return
 
-# TODO: Should implement token to authorize actions
 @app.get("/user", tags=["users"])
 def get_user(db: Session = Depends(get_db), user: User = Depends(current_active_user)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return {"user": {"username": user.username,
                      "email": user.email,}}
-
-# @app.get("/users/", tags=["users"])
-# def get_user_by_email(email: str, db: Session = Depends(get_db), user: User = Depends(current_active_user)):
-#     if not user:
-#         raise HTTPException(status_code=401, detail="Unauthorized")
-    
-#     user = crud.read_user_by_email(db, email)
-#     return {"user": {"email": user.email, "id": user.id}}
 
 @app.get("/users", tags=["users"], status_code=status.HTTP_200_OK)
 def get_all_users(db: Session = Depends(get_db), user: User = Depends(current_active_user)):
@@ -177,7 +170,6 @@ def create_user(body: schemas.UserCreate, db: Session = Depends(get_db)):
     user = crud.post_user(db, username, email, password)
     return {"username": username}
 
-# TODO: set_cookie method not working for localhost
 @app.post("/login", tags=["users"], status_code=status.HTTP_200_OK)
 async def user_login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     username = form_data.username
@@ -212,66 +204,12 @@ def user_logout(db: Session = Depends(get_db), user: User = Depends(current_acti
 """ OAuth2 Authentication and Authorization"""
 # tokenUrl is the URL the client use to send username and password to get a token
 
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
-
-# def hash_password(password):
-#     return pwd_context.hash(password)
-
-# def authenticate_user(fake_db, username: str, password: str):
-#     user = get_user(fake_db, username)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
-
-# # automatically check the Authorization header
-# @app.get("/test/")
-# async def read_tokens(token: str = Depends(oauth2_scheme)):
-#     return {"token": token}
-
-# fake_users_db = {
-#     "johndoe": {
-#         "username": "johndoe",
-#         "full_name": "John Doe",
-#         "email": "johndoe@example.com",
-#         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-#         "disabled": False,
-#     }
-# }
-
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class TokenData(BaseModel):
     username: Union[str, None] = None
-
-# class fake_User(BaseModel):
-#     username: str
-#     email: Union[str, None] = None
-#     full_name: Union[str, None] = None
-#     disabled: Union[bool, None] = None
-
-# class fake_UserInDB(fake_User):
-#     hashed_password: str
-
-# def fake_hash_password(password: str):
-#     return "fakehashed" + password
-
-# def get_user(db, username: str):
-#     if username in db:
-#         user_dict = db[username]
-#         return fake_UserInDB(**user_dict)
-
-# # def fake_decode_token(token):
-# #     # This doesn't provide any security at all
-# #     # Check the next version
-# #     user = get_user(fake_users_db, token)
-# #     return user
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
@@ -282,59 +220,5 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALORITHM)
     return encoded_jwt
-
-# async def get_current_user(token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     user = get_user(fake_users_db, username=token_data.username)
-#     return user
-
-# async def get_current_active_user(active_user: fake_User = Depends(get_current_user)):
-#     if active_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return active_user
-
-# @app.get("/test/me")
-# async def read_me(user: fake_User = Depends(get_current_active_user)):
-#     return user
-
-
-
-# @app.post("/token")
-# async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(
-#         data={"sub": user.username}, expires_delta=access_token_expires
-#     )
-#     return {"access_token": access_token, "token_type": "bearer"}
-
-SECRET_KEY = "871796f3497fd7ce65efd5d475c794a4723d2907b3c2b3730ca838070ca816d3"
-ALORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# class Token(BaseModel):
-#     access_token: str
-#     token_type: str
-
-# class TokenDate(BaseModel):
-#     username: Union[str, None] = None
 
 
